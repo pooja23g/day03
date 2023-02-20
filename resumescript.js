3c16394b1f718349f1bb5801157d99da475b9897
pooja23g <124581907+pooja23g@users.noreply.github.com>
2023-02-20 12:07:14 +0530

    Add files via upload

---
+++ resumescript.js
@@ -0,0 +1,40 @@
+var jsonresume={
+    "basics": {
+      "name": "Pooja.g",
+      "email": "pooja126@gamil.com",
+      "phone": 91221111769,
+      "degree": "B.E",
+      "location": {
+        "state": "Tamilnadu",
+        "country": "India"
+      }
+      
+        },
+    
+
+    "education": [
+      {
+        "institution": "Thiagarajar College Of Engineering",
+        "department": "B.E.E.C.E",
+        "studyType": "fulltime",
+        "batch start year": 2015,
+        "batch end year": 2019,
+        "gpa": 8.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginner",
+        
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+   
+  }
+  console.log(jsonresume);
+  
---
+++ script.js
@@ -0,0 +1,19 @@
+var studentname = [{
+    "name":"john doe",
+    "age":"32",
+    "year":"2018",
+    "address":"san fransico",
+},
+{
+    "name":"jack",
+    "age":"34",
+    "year":"2019",
+    "address":"Newyork",
+}];
+//for loop
+for(var i = 0; i < studentname.length; i++) {
+   
+
+    console.log(studentname[i].name,studentname[i].age,studentname[i].year,studentname[i].address);
+
+}
